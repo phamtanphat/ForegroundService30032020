3253073677729c45ac4c19c5279c9be7f03cf993
phatdroid94 <phatdroid94@gmail.com>
2020-05-27 21:29:15 +0700

    create service

--- app/src/main/java/com/example/foregroundservice30032020/MainActivity.java
+++ app/src/main/java/com/example/foregroundservice30032020/MainActivity.java
@@ -2,13 +2,29 @@ package com.example.foregroundservice30032020;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button mBtnOpen,mBtnClose;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mBtnClose = findViewById(R.id.buttonClose);
+        mBtnOpen = findViewById(R.id.buttonOpen);
+
+        mBtnOpen.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this,MyService.class);
+                startService(intent);
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/example/foregroundservice30032020/MyService.java
@@ -0,0 +1,29 @@
+package com.example.foregroundservice30032020;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+import android.widget.Toast;
+
+import androidx.annotation.Nullable;
+
+public class MyService extends Service {
+
+    @Nullable
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        Toast.makeText(this, "onCreate", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        Toast.makeText(this, "onStart Command", Toast.LENGTH_SHORT).show();
+        return START_STICKY;
+    }
+}
